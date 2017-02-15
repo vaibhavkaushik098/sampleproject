821ce2872c790d1afd5af825d6171fea7fa5136e
Vaibhav Kaushik <vaibhavkaushik098@gmail.com>
2017-02-15 14:34:59 +0530

    chapter 4

--- server/configure.js
+++ server/configure.js
@@ -9,15 +9,22 @@ methodOverride = require('method-override'),
 errorHandler = require('errorhandler');
 module.exports = function(app) {
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':true}));
-app.use(bodyParser.json());
+app.use(bodyParser({
+uploadDir:path.join(__dirname, 'public/upload/temp')
+}));
 app.use(methodOverride());
 app.use(cookieParser('some-secret-value-here'));
-routes(app);//moving the routes to routes folder.
+routes(app);
 app.use('/public/', express.static(path.join(__dirname,
 '../public')));
-if ('development' === app.get('env'))  {
+if ('development' === app.get('env')) {
 app.use(errorHandler());
 }
+app.engine('handlebars', exphbs.create({
+defaultLayout: 'main',
+layoutsDir: app.get('views') + '/layouts',
+partialsDir: [app.get('views') + '/partials']
+}).engine);
+app.set('view engine', 'handlebars');
 return app;
 };
