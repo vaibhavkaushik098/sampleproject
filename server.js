8082a395dbe3e579e69c30deecf1283fc3ad3455
abhishek <abhishek>
2017-02-16 18:02:21 +0530

    mongoose

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express'),
 config = require('./server/configure'),
+mongoose = require('mongoose');
 app = express();
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
@@ -7,6 +8,10 @@ app = config(app);
 //app.get('/', function(req, res){
 //res.send('Hello World');
 //});
-var server = app.listen(app.get('port'), function() {
+mongoose.connect('mongodb://localhost/imgPloadr'); 
+mongoose.connection.on('open', function() 
+	{ console.log('Mongoose connected.'); }); 
+var server = app.listen(app.get('port'), 
+	function() {
 console.log('Server up: http://localhost:' + app.get('port'));
 });
